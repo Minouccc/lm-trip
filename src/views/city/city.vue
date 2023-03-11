1fae06de926dce4809223725fe591b60bc80a5a5
Minouccc <343723899@qq.com>
2023-03-11 15:59:08 +0800

    add city-group component

--- src/views/city/city.vue
+++ src/views/city/city.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="city top-page">
-    <van-search
-      v-model="SearchValue"
-      placeholder="城市/区域/位置"
-      show-action
-      shape="round"
-      @cancel="cancelClick"
-    />
-    <van-tabs v-model:active="tabActive" line-height="3">
-      <template v-for="(value, key, index) in allCities" :key="key">
-        <van-tab :title="value.title"></van-tab>
+    <div class="top">
+      <van-search
+        v-model="SearchValue"
+        placeholder="城市/区域/位置"
+        show-action
+        shape="round"
+        @cancel="cancelClick"
+      />
+      <van-tabs v-model:active="tabActive" line-height="3">
+        <template v-for="(value, key, index) in allCities" :key="key">
+          <van-tab :title="value.title" :name="key"></van-tab>
+        </template>
+      </van-tabs>
+    </div>
+    <div class="content">
+      <template v-for="(value, key, index) in allCities">
+        <city-group v-show="tabActive === key" :group-data="value" />
       </template>
-    </van-tabs>
+    </div>
   </div>
 </template>
 
@@ -20,15 +27,33 @@ import { ref } from "vue";
 import { useRouter } from "vue-router";
 import useCityStore from "@/stores/modules/city";
 import { storeToRefs } from "pinia";
+import cityGroup from "./cpns/city-group.vue";
+// tab的切换
+const tabActive = ref();
+
+// 搜索框功能
 const router = useRouter();
 const SearchValue = ref("");
-const tabActive = ref(0);
-
 const cancelClick = () => {
   router.back();
 };
+// 从Store中获取数据
 const cityStore = useCityStore();
 cityStore.fetchAllCitiesData();
 const { allCities } = storeToRefs(cityStore);
+
+// 获取选中标签后的数据
+// const currentCity = computed(() => allCities.value[tabActive.value]);
 </script>
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.top {
+  position: relative;
+  z-index: 9;
+}
+.city {
+  .content {
+    height: calc(100vh - 98px);
+    overflow-y: auto;
+  }
+}
+</style>
---
+++ src/views/city/cpns/city-group.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="city-group">
+    <van-index-bar :index-list="indexList">
+      <van-index-anchor index="热门" />
+      <div class="list">
+        <template v-for="(city, index) in groupData.hotCities">
+          <div class="hot-city">
+            {{ city.cityName }}
+          </div>
+        </template>
+      </div>
+
+      <template v-for="(group, index) in groupData.cities">
+        <van-index-anchor :index="group.group"></van-index-anchor>
+        <template v-for="(city, indey) in group?.cities">
+          <van-cell :title="city.cityName" />
+        </template>
+      </template>
+    </van-index-bar>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+
+//定义props
+const props = defineProps({
+  groupData: {
+    type: Object,
+    default: () => ({}),
+  },
+});
+
+//动态索引
+const indexList = computed(() => {
+  const list = props.groupData.cities.map((item) => item.group);
+  list.unshift("#");
+  return list;
+});
+</script>
+<style lang="less" scoped>
+.city-group {
+  .list {
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    padding: 10px;
+    padding-right: 25px;
+    .hot-city {
+      width: 70px;
+      height: 28px;
+      border-radius: 14px;
+      font-size: 12px;
+      line-height: 28px;
+      text-align: center;
+      color: #000;
+      background-color: #fff4ec;
+      margin: 6px;
+    }
+  }
+}
+</style>
