4f72d467982b7c3889b9171333c00c61ec05e182
Minouccc <343723899@qq.com>
2023-03-14 20:06:51 +0800

    get detailInfos

--- src/views/detail/detail.vue
+++ src/views/detail/detail.vue
@@ -10,10 +10,21 @@
 </template>
 
 <script setup>
+import { ref, computed } from "vue";
+import { getDetailInfos } from "@/services";
 import { useRoute, useRouter } from "vue-router";
 
 const route = useRoute();
 const router = useRouter();
+const houseId = route.params.id;
+
+//发送网络请求获取数据
+const detailInfos = ref({});
+const mainPart = computed(() => detailInfos.value.mainPart);
+getDetailInfos(houseId).then((res) => {
+  detailInfos.value = res.data;
+  console.log(res);
+});
 
 //监听返回按钮的点击
 const onClickLeft = () => {
