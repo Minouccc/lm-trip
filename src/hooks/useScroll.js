b753839438fc5faba4fb24e63641c3449797f8d1
Minouccc <343723899@qq.com>
2023-03-14 14:42:05 +0800

    add throttle

--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,19 +1,21 @@
 import { ref, onMounted, onUnmounted } from "vue";
+import { throttle } from "underscore";
 
-export default function useScroll(reachBottomCB) {
+export default function useScroll() {
   const isReachBottom = ref(false);
   const clientHeight = ref(0);
   const scrollTop = ref(0);
   const scrollHeight = ref(0);
-  const scrollListenerHandler = () => {
+
+  const scrollListenerHandler = throttle(() => {
     clientHeight.value = document.documentElement.clientHeight;
     scrollTop.value = document.documentElement.scrollTop;
     scrollHeight.value = document.documentElement.scrollHeight;
     if (clientHeight.value + scrollTop.value >= scrollHeight.value) {
-      if (reachBottomCB) reachBottomCB();
+      console.log("滚动到底部了");
       isReachBottom.value = true;
     }
-  };
+  }, 100);
 
   onMounted(() => {
     window.addEventListener("scroll", scrollListenerHandler);
